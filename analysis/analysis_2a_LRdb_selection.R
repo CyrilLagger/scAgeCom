7927e20ed090efc0bfecc2329141f54bb4809fe5
Cyril Lagger <lagger.cyril@gmail.com>
2020-09-24 23:33:52 +0100

    Improved analysis_2 files

--- analysis/analysis_2a_LRdb_selection.R
+++ analysis/analysis_2a_LRdb_selection.R
@@ -31,6 +31,9 @@ category_sources <- c("PPI", "IUPHAR", "KEGG", "PMID", "CPDB", "ramilowski",
 
 category_DBs <- names(LR6db_source)
 
+## Save the curated database as a csv file ####
+write.csv(LR6db_curated, paste0(dir_data_analysis, "a2_LR6db_curated.csv"), row.names = FALSE)
+
 ## Clean up the SOURCE of each interaction ####
 LR_rm_sctensor <- c("SWISSPROT_STRING", "TREMBL_STRING")
 LR_rm_nichenet <- c("ppi_bidir_bidir", "ppi_bidir_bidir_go", "ppi_bidir_r",
@@ -80,7 +83,8 @@ plot_upset_full_dbs <- ComplexUpset::upset(
   category_DBs,
   min_size = 50
 )
-ggsave(filename = paste0(dir_data_analysis, "analysis_2_plot_upset_full_dbs.png"),
+plot_upset_full_dbs
+ggsave(filename = paste0(dir_data_analysis, "a2_plot_upset_full_dbs.png"),
        plot = plot_upset_full_dbs, scale = 2)
 
 plot_upset_full_sources <- ComplexUpset::upset(
@@ -88,7 +92,7 @@ plot_upset_full_sources <- ComplexUpset::upset(
   category_sources,
   min_size = 50
 )
-ggsave(filename = paste0(dir_data_analysis, "analysis_2_plot_upset_full_sources.png"),
+ggsave(filename = paste0(dir_data_analysis, "a2_plot_upset_full_sources.png"),
        plot = plot_upset_full_sources, scale = 2)
 
 plot_upset_curated_dbs <- ComplexUpset::upset(
@@ -104,7 +108,7 @@ plot_upset_curated_dbs <- ComplexUpset::upset(
   ),
   min_size = 20
 )
-ggsave(filename = paste0(dir_data_analysis, "analysis_2_plot_upset_curated_dbs.png"),
+ggsave(filename = paste0(dir_data_analysis, "a2_plot_upset_curated_dbs.png"),
        plot = plot_upset_curated_dbs, scale = 2)
 
 plot_upset_curated_sources <- ComplexUpset::upset(
@@ -120,7 +124,7 @@ plot_upset_curated_sources <- ComplexUpset::upset(
   ),
   min_size = 30
 )
-ggsave(filename = paste0(dir_data_analysis, "analysis_2_plot_upset_curated_sources.png"),
+ggsave(filename = paste0(dir_data_analysis, "a2_plot_upset_curated_sources.png"),
        plot = plot_upset_curated_sources, scale = 2)
 
 #UpSetR::upset(LR_full_upset[, ..category_DBs], nsets = 6, order.by = "freq", nintersects = 35)
@@ -128,114 +132,11 @@ ggsave(filename = paste0(dir_data_analysis, "analysis_2_plot_upset_curated_sourc
 #UpSetR::upset(LR_curated_upset[, ..category_DBs], nsets = 6, order.by = "freq", nintersects = 35)
 #UpSetR::upset(LR_curated_upset[, ..category_sources], nsets = 10, order.by = "freq", nintersects = 35)
 
+## Check orthology confidence of all interactions 
+ftable(LR6db_curated$RECEPTOR_1_CONF, LR6db_curated$LIGAND_1_CONF)
 
 
 
 
-#look at orthology
-#OK:
-table(LRdb_filtered$LIGAND_2_CONF)
-table(LRdb_filtered$RECEPTOR_3_CONF)
-# a few 0
-table(LRdb_filtered$LIGAND_1_CONF)
-table(LRdb_filtered$RECEPTOR_1_CONF)
-table(LRdb_filtered$RECEPTOR_2_CONF)
-ftable(LRdb_filtered$LIGAND_1_CONF, LRdb_filtered$RECEPTOR_1_CONF)
-ftable(LRdb_filtered$LIGAND_1_CONF, LRdb_filtered$RECEPTOR_1_CONF, LRdb_filtered$RECEPTOR_2_CONF)
 
 
-
-library(data.table)
-fwrite(LRdb_filtered, file = "../../../../../test.csv")
-
-#annotation with GO
-library(clusterProfiler)
-library(org.Mm.eg.db)
-
-LR_genes <- unique(unlist(LR6db$LR6db_curated[, c("LIGAND_1", "LIGAND_2", "RECEPTOR_1", "RECEPTOR_2", "RECEPTOR_3")]))
-LR_genes <- LR_genes[!is.na(LR_genes)]
-
-
-LR_ggo_bc_l3 <- groupGO(
-  gene = LR_genes,
-  OrgDb = org.Mm.eg.db,
-  keyType = "SYMBOL",
-  ont = "BP",
-  level = 3,
-  readable = FALSE
-)@result
-
-LR_ggo_bc_l2 <- groupGO(
-  gene = LR_genes,
-  OrgDb = org.Mm.eg.db,
-  keyType = "SYMBOL",
-  ont = "BP",
-  level = 2,
-  readable = FALSE
-)@result
-
-LR_ggo_bc_l4 <- groupGO(
-  gene = LR_genes,
-  OrgDb = org.Mm.eg.db,
-  keyType = "SYMBOL",
-  ont = "BP",
-  level = 4,
-  readable = FALSE
-)@result
-
-####
-
-library(biomaRt)
-
-mart <- biomaRt::useMart(
-  "ensembl",
-  dataset = "mmusculus_gene_ensembl"
-)
-LR_genes_go <- biomaRt::getBM(
-  attributes = c(
-    "mgi_symbol",
-    "go_id",
-    "name_1006", 
-    "namespace_1003"
-  ),
-  filters = "mgi_symbol",
-  mart = mart,
-  values = LR_genes
-)
-setDT(LR_genes_go)
-
-LR_genes_go_comb <- LR_genes_go[, list(text = paste(name_1006, collapse=",")), by = mgi_symbol]
-
-LR_genes_go_comb2 <- LR_genes_go[, list(text = paste(mgi_symbol, collapse=",")), by = name_1006]
-
-library(GO.db)
-Term("GO:0016021")
-t <- c(GOBPOFFSPRING[["GO:0042110"]], "GO:0042110")
-Term("GO:0042110")
-Term(t[2])
-
-library(data.table)
-setDT(LR_genes_go)
-
-LR_genes_go[, count := .N, by = go_id]
-
-
-LR_genes_go[ go_id == "GO:0050789"]
-
-test <- LRall
-test2 <- test[ cpdb == TRUE | scsr == TRUE]
-
-#####
-upset_list_full_sources <- sapply(category_sources, function(i) {
-  LR6db_full[grepl(i, SOURCE_CLEAN)]$LR_SORTED
-})
-upset_list_full_DBs <- sapply(category_DBs, function(i) {
-  LR6db_full[grepl(i, DATABASE)]$LR_SORTED
-})
-
-upset_list_curated_sources <- sapply(category_sources, function(i) {
-  LR6db_curated[grepl(i, SOURCE_CLEAN)]$LR_SORTED
-})
-upset_list_curated_DBs <- sapply(category_DBs, function(i) {
-  LR6db_curated[grepl(i, DATABASE)]$LR_SORTED
-})
