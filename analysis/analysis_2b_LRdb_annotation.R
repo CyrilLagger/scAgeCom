3764f770d811ef3a8f662759f4b03bf92d6d2a09
Cyril Lagger <lagger.cyril@gmail.com>
2020-09-25 00:45:26 +0100

    Improved analysis files

--- analysis/analysis_2b_LRdb_annotation.R
+++ analysis/analysis_2b_LRdb_annotation.R
@@ -13,24 +13,14 @@
 ####################################################
 ##
 
-## 0. Library installation (to do only once and then this part can be removed) ####
-
-#very convenient package that extend the "data.frame" type
-install.packages("data.table")
-
-#"biomarRT" give a way to query Ensembl information directly from R (belongs to BioConductor, so require BiocManager)
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("biomaRt")
-
-#our package
-devtools::install_github("https://github.com/CyrilLagger/scDiffCom")
-
-## 1.  Load libraries ####
+## 1. Load libraries ####
 
 library(scDiffCom)
 library(data.table)
 library(biomaRt)
+
+# Analysis data path
+dir_data_analysis <- "../data_scAgeCom/analysis/"
 
 ## 2. Load the curated ligand-receptor interactions ####
 
@@ -62,14 +52,14 @@ LR_terms <- sort(unique(c(LR_annotation, LR_family, LR_subfamily)))
 #we can display them in the console
 LR_terms
 
-## 5. Get the Ensembl terms (GO, family or else) 
+## 5. Get the Ensembl terms (GO, family or else) ####
 
 #create an access to ensembl database via biomart (might take a minute or so)
 mart <- biomaRt::useMart(
   "ensembl",
   dataset = "mmusculus_gene_ensembl"
 )
-#retrive the information we want for our genes of interest
+#retrieve the information we want for our genes of interest
 LR_genes_info <- biomaRt::getBM(
   attributes = c(
     "mgi_symbol",
@@ -88,14 +78,24 @@ LR_genes_info <- biomaRt::getBM(
 )
 setDT(LR_genes_info)
 
-saveRDS(LR_genes_info, "../data_scAgeCom/analysis/analysis_2_LR_genes_info.rds")
+saveRDS(LR_genes_info, paste0(dir_data_analysis, "analysis_2_LR_genes_info.rds"))
 
 #display the first 10 rows
 head(LR_genes_info, 10)
 
+## 6. Create a table of the GO/family term frequency ####
 
+LR_go_frequency <- LR_genes_info[, .N, by = c("name_1006", "namespace_1003", "go_id")]
+LR_go_frequency <- LR_go_frequency[order(-N)]
+LR_go_frequency_top <- LR_go_frequency[N >= 30]
+LR_go_frequency_top <- LR_go_frequency_top[-c(1:11)]
 
-## 6. Combine the GO terms (one row per gene)
+LR_family_frequency <- LR_genes_info[, .N, by = c("family_description")]
+LR_family_frequency <- LR_family_frequency[order(-N)]
+
+LR_genes_info_filtered <- LR_genes_info[name_1006 %in% LR_go_frequency_top$name_1006]
+
+## 7. Combine the GO and family terms (one row per gene) ####
 
 LR_genes_go <- as.data.table(
   sapply(unique(LR_genes_info$mgi_symbol), function(i) {
@@ -108,9 +108,16 @@ LR_genes_go <- as.data.table(
 )
 colnames(LR_genes_go) <- c("gene", "GO_terms")
 
-saveRDS(LR_genes_go, "../data_scAgeCom/analysis/analysis_2_LR_GO_terms.rds")
-
-## 7. Combine the family terms (one row per gene)
+LR_genes_go_filtered <- as.data.table(
+  sapply(unique(LR_genes_info_filtered$mgi_symbol), function(i) {
+    temp <- LR_genes_info_filtered[mgi_symbol == i]
+    res <- paste0(sort(unique(temp$name_1006)), collapse = ",")
+    return(res)
+  },
+  USE.NAMES = TRUE),
+  keep.rownames = TRUE
+)
+colnames(LR_genes_go_filtered) <- c("gene", "GO_terms")
 
 LR_genes_family <- as.data.table(
   sapply(unique(LR_genes_info$mgi_symbol), function(i) {
@@ -123,34 +130,75 @@ LR_genes_family <- as.data.table(
 )
 colnames(LR_genes_family) <- c("gene", "family_terms")
 
-saveRDS(LR_genes_family, "../data_scAgeCom/analysis/analysis_2_LR_family_terms.rds")
-
-## 8. Merge the two previous tables in a single table
 LR_genes_go_family <- merge.data.table(
   LR_genes_go,
   LR_genes_family,
   by = "gene",
   all = TRUE
 )
+saveRDS(LR_genes_go_family, paste0(dir_data_analysis, "analysis_2_LR_GO-family_terms.rds"))
 
-saveRDS(LR_genes_go_family, "../data_scAgeCom/analysis/analysis_2_LR_GO-family_terms.rds")
 
 
-## 9. Create a table of the GO/family term frequency ####
+## 8. #####
+existing_colnames <- c(paste0("LIGAND_", 1:2), paste0("RECEPTOR_", 1:3))
+new_colnames_2 <- paste0(existing_colnames, "_GO_terms_2")
 
-LR_go_frequency <- LR_genes_info[, .N, by = c("name_1006", "namespace_1003", "go_id")]
-LR_go_frequency <- LR_go_frequency[order(-N)]
+LR_interactions[, (new_colnames) := lapply(existing_colnames, function(col) {
+    LR_genes_go[.SD, on = paste0("gene==", col), x.GO_terms]
+})]
 
-LR_family_frequency <- LR_genes_info[, .N, by = c("family_description")]
-LR_family_frequency <- LR_family_frequency[order(-N)]
+LR_interactions[, (new_colnames_2) := lapply(existing_colnames, function(col) {
+  LR_genes_go[.SD, on = paste0("gene==", col), x.GO_terms]
+})]
 
-#each table can be filtered as followed
-#LR_go_frequency_filt <- LR_go_frequency[N >= 7]
+LR_interactions[, GO_intersection := sapply(1:nrow(.SD), function(i) {
+  temp_L <- sapply(paste0("LIGAND_", 1:2, "_GO_terms"), function(col) {
+    unlist(strsplit(get(col)[[i]], ",", fixed = TRUE))
+  })
+  temp_L <- temp_L[!is.na(temp_L)]
+  temp_L <- unlist(temp_L)
+  temp_R <- sapply(paste0("RECEPTOR_", 1:3, "_GO_terms"), function(col) {
+    unlist(strsplit(get(col)[[i]], ",", fixed = TRUE))
+  })
+  temp_R <- temp_R[!is.na(temp_R)]
+  temp_R <- unlist(temp_R)
+  temp_inter <- intersect(temp_L, temp_R)
+  temp_inter <- paste0(temp_inter, collapse = ",")
+}) ]
+
+LR_interactions[, GO_union := sapply(1:nrow(.SD), function(i) {
+  temp_L <- sapply(paste0("LIGAND_", 1:2, "_GO_terms"), function(col) {
+    unlist(strsplit(get(col)[[i]], ",", fixed = TRUE))
+  })
+  temp_L <- temp_L[!is.na(temp_L)]
+  temp_L <- unlist(temp_L)
+  temp_R <- sapply(paste0("RECEPTOR_", 1:3, "_GO_terms"), function(col) {
+    unlist(strsplit(get(col)[[i]], ",", fixed = TRUE))
+  })
+  temp_R <- temp_R[!is.na(temp_R)]
+  temp_R <- unlist(temp_R)
+  temp_union <- union(temp_L, temp_R)
+  temp_union <- paste0(temp_union, collapse = ",")
+  
+}) ]
+
+LR_interactions[GO_intersection == ""]$LR_SORTED
+LR_interactions[GO_union == ""]
+
+test <- LR_interactions[, c("LR_SORTED", "LIGAND_1_GO_terms", "LIGAND_1_GO_terms_2", "GO_intersection")]
+
+unique(LR_genes[!(LR_genes %in% LR_genes_go$gene)])
+
+######################
+
+## 5. Add new columns with the intersection and union of the GO terms (takes one minute or so)
+
 
 
 ##### Below: code for testing ####
 
-## 10. On how to extract group of genes with specific terms ####
+## 8. On how to extract group of genes with specific terms ####
 
 #Example: let's retrieve all the go terms with "immune" and all the genes with "immune"
 go_immune <- LR_go_frequency[grepl("immune", name_1006)]
--- analysis/analysis_3_run_scDiffCom.R
+++ analysis/analysis_3_run_scDiffCom.R
@@ -2,7 +2,7 @@
 ##
 ## Project: scAgeCom
 ##
-## cyril.lagger@liverpool.ac.uk - July 2020
+## cyril.lagger@liverpool.ac.uk - September 2020
 ##
 ## Apply scDiffCom to all three datasets.
 ##
@@ -16,28 +16,18 @@ library(scDiffCom)
 library(foreach)
 library(doParallel)
 
-## Parameters ####
+## Set up fixed parameters that should not change ####
 
-#Note change each time before running the analysis
-
-dataset <- "tms_droplet" # c("tms_facs", "tms_droplet", "calico")
-LR_db <- "mixed" # c("all", "scsr", "sctensor", "nichenet", "mixed")
-normalization <- "size_factor" #c("size_factor", "sctransform")
+normalization <- "size_factor"
+min_cells <- 5
 is_log <- TRUE
 n_iter <- 10000
-var_sex <- "male" #c("default", "female", "male")
-run_test <- FALSE
-calico_subtype <- FALSE
 dir_data_output <- getwd()
+run_test <- FALSE
 
-if(dataset == "calico" & var_sex != "default") {
-  message("Calico dataset has only male mice. Setting variable 'var_sex' to 'default'.")
-  var_sex <- "default"
-}
+## Path where the Seurat files are stored ####
 
-## Data path ####
-
-paths <- c(
+dataset_paths <- c(
   tms_facs = "/home/nis/zabidi/work/lcyril_data/scRNA_seq/seurat_processed/seurat_tms_facs.rds",
   tms_droplet = "/home/nis/zabidi/work/lcyril_data/scRNA_seq/seurat_processed/seurat_tms_droplet.rds",
   calico_kidney = "/home/nis/zabidi/work/lcyril_data/scRNA_seq/seurat_processed/seurat_calico_kidney.rds",
@@ -45,138 +35,130 @@ paths <- c(
   calico_spleen = "/home/nis/zabidi/work/lcyril_data/scRNA_seq/seurat_processed/seurat_calico_spleen.rds"
 )
 
-## Create output path ####
+## Load the curated LR interactions ####
 
-if(is_log) {
-  if(calico_subtype) {
-    output_dir <- paste0(dir_data_output, "/diffcom_", dataset, "_subtype_", normalization, "_log_", n_iter, "iter_", LR_db)
-  } else {
-    output_dir <- paste0(dir_data_output, "/diffcom_", dataset, "_", normalization, "_log_", n_iter, "iter_", LR_db)
+LR6db_curated <- scDiffCom::LR6db$LR6db_curated
+
+## List of analysis to do over datasets and sex
+
+analysis_list <- list(
+  list(dataset = "calico", type = "default"),
+  list(dataset = "calico", type = "subtype"),
+  list(dataset = "tms_facs", type = "mixed"),
+  list(dataset = "tms_facs", type = "female"),
+  list(dataset = "tms_facs", type = "male"),
+  list(dataset = "tms_facs", type = "sex"),
+  list(dataset = "tms_droplet", type = "mixed"),
+  list(dataset = "tms_droplet", type = "female"),
+  list(dataset = "tms_droplet", type = "male"),
+  list(dataset = "tms_droplet", type = "sex")
+)
+
+## Do the analysis ####
+
+for(analysis in analysis_list) {
+  message(paste0("Start analysis of ", analysis$dataset, "_", analysis$type))
+  output_dir <- paste0(dir_data_output, "/scdiffcom_",
+                       analysis$dataset, "_", analysis$type, "_",
+                       normalization, "_log", is_log, "_", n_iter, "iter")
+  if(!dir.exists(output_dir)) {
+    message("Create output directory.")
+    dir.create(output_dir)
   }
-} else {
-  if(calico_subtype) {
-    output_dir <- paste0(dir_data_output, "/diffcom_", dataset, "_subtype_", normalization, "_nlog_", n_iter, "iter_", LR_db)
-  } else {
-    output_dir <- paste0(dir_data_output, "/diffcom_", dataset, "_", normalization, "_nlog_", n_iter, "iter_", LR_db)
-  }
-}
-if(var_sex != "default") {
-  output_dir <- paste0(output_dir, "_", var_sex)
-}
-if(run_test) {
-  output_dir <- paste0(output_dir, "_test")
-}
-
-if(!dir.exists(output_dir)) {
-  message("Create output directory.")
-  dir.create(output_dir)
-}
-
-## Read LR-pairs ####
-
-message("Read ligand-receptor database.")
-LR <- LRall
-if(LR_db == "all") {
-  LR <- LR[, c("GENESYMB_L","GENESYMB_R", "SYMB_LR")]
-} else if(LR_db == "mixed") {
-  LR <- LR[LR$scsr == TRUE | LR$cpdb == TRUE, c("GENESYMB_L","GENESYMB_R", "SYMB_LR")]
-} else {
-  LR <- LR[LR[[LR_db]], c("GENESYMB_L","GENESYMB_R", "SYMB_LR")]
-}
-
-## Read Seurat object ####
-message("Read seurat object.")
-if(dataset %in% c("tms_facs", "tms_droplet")) {
-  seurat_obj <- readRDS(paths[[dataset]])
-  seurat_obj$age_group <- ifelse(seurat_obj$age %in% c('1m', '3m'), 'young', 'old')
-  if(var_sex == "female") {
-    seurat_obj <- subset(seurat_obj, subset = sex == "female")
-  } else if(var_sex == "male") {
-    seurat_obj <- subset(seurat_obj, subset = sex == "male")
-  } else if(var_sex != "default") {
-    stop("Variable var_sex has to be one of the following: 'default', 'female' or 'male'.")
-  }
-  md_temp <- seurat_obj@meta.data
-  tokeep <- apply(
-    table(as.character(md_temp$tissue), md_temp$age_group) >= 5,
-    MARGIN = 1,
-    FUN = all
-  )
-  tissue_list <- names(tokeep[tokeep])
-  n_tissue <- length(tissue_list)
-} else if(dataset == "calico") {
-  seurat_kidney <- readRDS(paths[["calico_kidney"]])
-  seurat_kidney$age_group <- as.character(seurat_kidney$age)
-  seurat_lung <- readRDS(paths[["calico_lung"]])
-  seurat_lung$age_group <- as.character(seurat_lung$age)
-  seurat_spleen <- readRDS(paths[["calico_spleen"]])
-  seurat_spleen$age_group <- as.character(seurat_spleen$age)
-  tissue_list <- c("Kidney", "Lung", "Spleen")
-  seurat_list <- list(
-    Kidney = seurat_kidney,
-    Lung = seurat_lung,
-    Spleen = seurat_spleen
-    )
-  n_tissue <- 3
-} else {
-  stop("Dataset not supported.")
-}
-
-if(run_test){
-  tissue_list <- tissue_list[1:2]
-  n_tissue <- 2
-}
-
-## Setup parallel environment ####
-
-message("Setup parallel environment.")
-registerDoParallel(cores= min(n_tissue, 30))
-
-## Actual analysis ####
-
-message("Start tissue per tissue scDiffCom analysis")
-foreach(i = 1:n_tissue, .packages = c("Seurat", "scDiffCom")) %dopar% {
-  tiss <- tissue_list[i]
-  message(paste0("Analysis of the ", tiss, ". Tissue ", i, " out of ", n_tissue, "."))
-  if(dataset %in% c("tms_facs", "tms_droplet")) {
-    message("Subset Seurat object")
-    cells_tiss <- colnames(seurat_obj)[which(seurat_obj$tissue == tiss)]
-    seurat_tiss <- subset(seurat_obj, cells = cells_tiss)
-    cell_type_id <- "cell_ontology_class"
-  } else if(dataset == "calico") {
-    seurat_tiss <- seurat_list[[tiss]]
-    if(calico_subtype) {
-      cell_type_id <- "subtype"
-    } else {
-      cell_type_id <- "cell_type"
+  message("Read seurat object.")
+  if(analysis$dataset %in% c("tms_facs", "tms_droplet")) {
+    seurat_obj <- readRDS(dataset_paths[[analysis$dataset]])
+    seurat_obj$age_group <- ifelse(seurat_obj$age %in% c('1m', '3m'), 'YOUNG', 'OLD')
+    if(analysis$type == "female") {
+      seurat_obj <- subset(seurat_obj, subset = sex == "female")
     }
-  } else {
-    stop("Dataset not supported.")
+    if(analysis$type == "male") {
+      seurat_obj <- subset(seurat_obj, subset = sex == "male")
+    }
+    if(analysis$type == "sex") {
+      group_filter <- "sex"
+    } else {
+      group_filter <- "age_group"
+    }
+    md_temp <- seurat_obj@meta.data
+    tokeep <- apply(
+      table(as.character(md_temp$tissue), as.character(md_temp[[group_filter]])) >= min_cells,
+      MARGIN = 1,
+      FUN = all
+    )
+    tissue_list <- names(tokeep[tokeep])
+    n_tissue <- length(tissue_list)
   }
-  if(normalization == "size_factor") {
+  if(analysis$dataset == "calico") {
+    seurat_kidney <- readRDS(dataset_paths[["calico_kidney"]])
+    seurat_kidney$age_group <- ifelse(seurat_kidney$age == "young", 'YOUNG', 'OLD')
+    seurat_lung <- readRDS(dataset_paths[["calico_lung"]])
+    seurat_lung$age_group <- ifelse(seurat_lung$age == "young", 'YOUNG', 'OLD')
+    seurat_spleen <- readRDS(dataset_paths[["calico_spleen"]])
+    seurat_spleen$age_group <- ifelse(seurat_spleen$age == "young", 'YOUNG', 'OLD')
+    tissue_list <- c("Kidney", "Lung", "Spleen")
+    seurat_list <- list(
+      Kidney = seurat_kidney,
+      Lung = seurat_lung,
+      Spleen = seurat_spleen
+    )
+    n_tissue <- 3
+  }
+  if(run_test){
+    tissue_list <- tissue_list[1:2]
+    n_tissue <- 2
+  }
+  message("Setup parallel environment.")
+  registerDoParallel(cores= min(n_tissue, 30))
+  message("Start tissue per tissue scDiffCom analysis (in parallel).")
+  foreach(i = 1:n_tissue, .packages = c("Seurat", "scDiffCom")) %dopar% {
+    tiss <- tissue_list[i]
+    message(paste0("Analysis of the ", tiss, ". Tissue ", i, " out of ", n_tissue, "."))
+    if(analysis$dataset %in% c("tms_facs", "tms_droplet")) {
+      message("Subset Seurat object")
+      cells_tiss <- colnames(seurat_obj)[which(seurat_obj$tissue == tiss)]
+      seurat_tiss <- subset(seurat_obj, cells = cells_tiss)
+      cell_type_id <- "cell_ontology_class"
+      if(analysis$type == "sex") {
+        condition_id == "sex"
+      } else {
+        condition_id == "age_group"
+      }
+    } else if(analysis$dataset == "calico") {
+      seurat_tiss <- seurat_list[[tiss]]
+      condition_id <- "age_group"
+      if(analysis$type == subtype) {
+        cell_type_id <- "subtype"
+      } else {
+        cell_type_id <- "cell_type"
+      }
+    }
     Seurat::DefaultAssay(seurat_tiss) <- "RNA"
     message("Size-factor normalization:")
     seurat_tiss <- Seurat::NormalizeData(seurat_tiss, assay = "RNA")
     dt_res <- scDiffCom::run_diffcom(
       seurat_obj = seurat_tiss,
-      LR_data = LR,
+      LR_data = LR6db_curated,
       seurat_cell_type_id = cell_type_id,
-      condition_id = "age_group",
+      condition_id = condition_id,
       assay = "RNA",
       slot = "data",
       log_scale = is_log,
-      min_cells = 5,
+      min_cells = min_cells,
       threshold = 0.1,
       permutation_analysis = TRUE,
       one_sided = FALSE,
       iterations = n_iter,
-      return_distr = FALSE
+      return_distr = FALSE,
+      seed = 42
     )
     message(paste0("Saving results for the ", tiss, "."))
-    saveRDS(dt_res, file = paste0(output_dir, "/diffcom_", tiss, ".rds"))
-    
-  } else if(normalization == "sct") {
-    stop("To do later on.")
+    saveRDS(dt_res, file = paste0(output_dir, "/scdiffcom_", tiss, ".rds"))
+    NULL
   }
-  NULL
 }
+
+
+
+
+
