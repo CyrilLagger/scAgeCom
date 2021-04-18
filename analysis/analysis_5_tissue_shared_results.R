8c0ca0fc7fb27b3453ed17793b7e6f242a97dafd
Cyril Lagger <lagger.cyril@gmail.com>
2021-04-18 10:07:58 +0100

    Added total tissue counts in counts table
    Former-commit-id: d0d991c1ed072c4c0e43f4de73cd571abc1ca612

--- analysis/analysis_5_tissue_shared_results.R
+++ analysis/analysis_5_tissue_shared_results.R
@@ -180,6 +180,8 @@ ORA_KEYWORD_COUNTS[
   c("ASPECT", "GO Level") := mget(paste0("i.", c("ASPECT", "GO Level")))
 ]
 
+ORA_KEYWORD_COUNTS[, `GO Level` := factor(`GO Level`)]
+
 setkey(ORA_KEYWORD_COUNTS)
 setorder(
   ORA_KEYWORD_COUNTS,
@@ -188,40 +190,55 @@ setorder(
 
 ORA_KEYWORD_COUNTS[
   ,
-  `Overall (Union)`:= ifelse(
-    `Overall (Union)` < 10 & `Overall (Union)` > 0 ,
-    paste0("0", `Overall (Union)`, "/23"),
-    paste0(`Overall (Union)`, "/23")
+  `Overall (Union)`:= factor(
+    ifelse(
+      
+      `Overall (Union)` < 10 & `Overall (Union)` > 0 ,
+      paste0("0", `Overall (Union)`, "/23"),
+      paste0(`Overall (Union)`, "/23")
+    )
   ),
 ]
 ORA_KEYWORD_COUNTS[
   ,
-  `TMS FACS (male)`:= ifelse(
-    `TMS FACS (male)` < 10 & `TMS FACS (male)` > 0 ,
-    paste0("0", `TMS FACS (male)`, "/21"),
-    paste0(`TMS FACS (male)`, "/21")
+  `TMS FACS (male)`:= factor(
+    ifelse(
+      `TMS FACS (male)` < 10 & `TMS FACS (male)` > 0 ,
+      paste0("0", `TMS FACS (male)`, "/21"),
+      paste0(`TMS FACS (male)`, "/21")
+    )
   ),
 ]
 ORA_KEYWORD_COUNTS[
   ,
-  `TMS FACS (female)`:= ifelse(
-    `TMS FACS (female)` < 10 & `TMS FACS (female)` > 0 ,
-    paste0("0", `TMS FACS (female)`, "/19"),
-    paste0(`TMS FACS (female)`, "/19")
+  `TMS FACS (female)`:= factor(
+    ifelse(
+      `TMS FACS (female)` < 10 & `TMS FACS (female)` > 0 ,
+      paste0("0", `TMS FACS (female)`, "/19"),
+      paste0(`TMS FACS (female)`, "/19")
+    )
   ),
 ]
 ORA_KEYWORD_COUNTS[
   ,
-  `TMS Droplet (male)`:= paste0(`TMS Droplet (male)`, "/6"),
+  `TMS Droplet (male)`:= factor(
+    paste0(`TMS Droplet (male)`, "/6")
+  ),
 ]
 ORA_KEYWORD_COUNTS[
   ,
-  `TMS Droplet (female)` :=  paste0(`TMS Droplet (female)`, "/9"),
+  `TMS Droplet (female)` :=  factor(
+    paste0(`TMS Droplet (female)`, "/9")
+  ),
 ]
 ORA_KEYWORD_COUNTS[
   ,
-  Calico2019 := paste0(Calico2019, "/3"),
+  Calico2019 := factor(
+    paste0(Calico2019, "/3")
+  ),
 ]
+
+
 
 # ORA_KEYWORD_COUNTS[
 #   ,
@@ -278,11 +295,20 @@ build_KEYWORD_COUNTS_display <- function(
   }
   DT <- DT::datatable(
     data = dt,
+    filter = list(
+      position ="top",
+      clear = FALSE,
+      plain = FALSE
+    ),
+    class = "display compact",
     options =list(
-      pageLength = 10
+      pageLength = 10,
+      columnDefs = list(
+        list(width = '300px', targets = c(1))
+      )
     ),
     caption = tags$caption(
-      style = 'caption-side: top; text-align: center; color:black; font-size:150% ;',
+      style = 'caption-side: top; text-align: center; color:black; font-size:100% ;',
       paste0(
         "Number of tissues in which ",
         category,
@@ -291,7 +317,11 @@ build_KEYWORD_COUNTS_display <- function(
         " CCIs."
       )
     )
-  )
+  )  %>%
+    DT::formatStyle(
+      colnames(dt)[-1],
+      `text-align` = 'center'
+    )
   if (category == "GO Terms") {
     DT <- DT %>% DT::formatStyle(c(7), `border-right` = "solid 2px")
   }
@@ -390,6 +420,7 @@ ORA_KEYWORD_SUMMARY[
   on = "VALUE",
   c("ASPECT", "GO Level") := mget(paste0("i.", c("ASPECT", "GO Level")))
 ]
+ORA_KEYWORD_SUMMARY[, `GO Level` := factor(`GO Level`)]
 
 ORA_KEYWORD_TEMPLATE <- unique(
   ORA_KEYWORD_SUMMARY[
@@ -407,7 +438,7 @@ plot_KEYWORD_SUMMARY <- function(
   category,
   keyword
 ) {
-  dt <- ora_keyword_summary[
+  dt <- copy(ora_keyword_summary)[
     ORA_CATEGORY == category
   ]
   if (!(keyword %in% dt$VALUE)) {
@@ -416,7 +447,7 @@ plot_KEYWORD_SUMMARY <- function(
   dt <- dt[
     VALUE == keyword
   ]
-  dt <- ora_keyword_template[
+  dt <- copy(ora_keyword_template)[
     dt,
     on = c("Tissue", "Dataset"),
     Regulation := i.ORA_REGULATION
@@ -474,8 +505,8 @@ plot_KEYWORD_SUMMARY <- function(
     xlab("") +
     ylab("") +
     theme(plot.title = element_text(hjust = 0.5)) +
-    theme(text=element_text(size = 18)) +
-    theme(axis.text=element_text(size = 18))
+    theme(text=element_text(size = 12)) +
+    theme(axis.text=element_text(size = 12))
   plotly::ggplotly(
     p,
     source = "TCA_PLOT_KEYWORD_SUMMARY",
@@ -498,6 +529,21 @@ ALL_ORA_CATEGORIES_GLOBAL <- c(
   "By Cell Type Families"
 )
 
+
+ALL_ORA_CATEGORIES_KEYWORD <- c(
+  "LRIs",
+  "Ligand Gene(s)",
+  "Receptor Gene(s)",
+  #"ER Cell Types",
+  #"Emitter Cell Types",
+  #"Receiver Cell Types",
+  "GO Terms",
+  "KEGG Pathways",
+  "ER Cell Families",
+  "Emitter Cell Families",
+  "Receiver Cell Families"
+)
+
 ## save all results ####
 
 data_5_tissue_shared_results <- list(
@@ -505,6 +551,7 @@ data_5_tissue_shared_results <- list(
   ORA_KEYWORD_SUMMARY = ORA_KEYWORD_SUMMARY,
   ORA_KEYWORD_TEMPLATE = ORA_KEYWORD_TEMPLATE,
   ALL_ORA_CATEGORIES_GLOBAL = ALL_ORA_CATEGORIES_GLOBAL,
+  ALL_ORA_CATEGORIES_KEYWORD = ALL_ORA_CATEGORIES_KEYWORD,
   build_KEYWORD_COUNTS_display = build_KEYWORD_COUNTS_display,
   plot_KEYWORD_SUMMARY = plot_KEYWORD_SUMMARY
 )
