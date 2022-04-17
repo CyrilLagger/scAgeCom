55c261107eccf87445eae4145b6c199e439bf2d0
Cyril Lagger <lagger.cyril@gmail.com>
2022-04-17 09:11:41 +0000

    Start aging database analysis
    Former-commit-id: b8fad2bdb7a0c4db2c2f896d92e13f1738cdd6f9

---
+++ analysis/analysis_11_aging_db.R
@@ -0,0 +1,16 @@
+####################################################
+##
+## Project: scAgeCom
+##
+## Last update - April 2022
+##
+## cyril.lagger@liverpool.ac.uk
+## ursu_eugen@hotmail.com
+## anais.equey@etu.univ-amu.fr
+##
+## Overlap scAgeCom results
+## with PubMed, GenAge, CellAge, etc
+##
+####################################################
+##
+
