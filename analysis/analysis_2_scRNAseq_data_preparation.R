01d3d57c8d0c12dbaf6233fc71c4bef68606e21d
Cyril Lagger <lagger.cyril@gmail.com>
2022-05-14 17:36:55 +0000

    Rename gene names based on MGI
    Former-commit-id: 1f7185af22464d07092bc76167228053b0883ee1

--- analysis/analysis_2_scRNAseq_data_preparation.R
+++ analysis/analysis_2_scRNAseq_data_preparation.R
@@ -16,19 +16,7 @@
 ## Libraries ####
 
 library(Seurat)
-library(data.table)
-
-## Specific paths ####
-
-path_project <- "/workspace/postdoc_aging/projects/all_projects/"
-path_scagecom_input <- paste0(
-  path_project,
-  "P1_scInterComAging/data_scAgeCom/input/"
-)
-path_scagecom_output <- paste0(
-  path_project,
-  "P1_scInterComAging/data_scAgeCom/output/"
-)
+library(org.Mm.eg.db)
 
 ## Dataset Overview ####
 
@@ -344,7 +332,7 @@ seurats_unprocessed$calico_spleen$cell_family <-
 seurats_unprocessed$calico_spleen$cell_abbreviation <-
   md_calico_spleen$cell_abbreviation
 
-## save new Seurat objects ####
+## save Processed Seurat objects ####
 
 saveRDS(
   seurats_unprocessed$tms_facs,
@@ -409,11 +397,381 @@ seurats_processed <- list(
   calico_spleen = readRDS(paths_dataset_processed[["calico_spleen"]])
 )
 
+## Mapping gene names to MGI ####
+
+genes_seurat <- lapply(
+  seurats_processed,
+  rownames
+)
+
+mart_seurats <- biomaRt::getBM(
+  attributes = c(
+    "mgi_symbol",
+    "entrezgene_id",
+    "ensembl_gene_id"
+  ),
+  filters = "mgi_symbol",
+  mart = mart_db_mouse,
+  values = unique(c(unlist(genes_seurat), genes_lri_mouse))
+)
+setDT(mart_seurats)
+
+columns(org.Mm.eg.db)
+dt_org_mm <- select(
+  org.Mm.eg.db,
+  keys = keys(org.Mm.eg.db),
+  columns = c("SYMBOL", "ENSEMBL", "ENTREZID", "MGI", "ALIAS")
+)
+setDT(dt_org_mm)
+
+table(genes_lri_mouse %in% dt_org_mm$SYMBOL)
+table(genes_lri_mouse %in% dt_org_mm$ALIAS)
+
+table(genes_seurat$tms_facs %in% dt_org_mm$ALIAS)
+
+dt_genes_seurat <- rbindlist(
+  l = lapply(
+    genes_seurat,
+    function(i) {
+      data.table(gene_orig = i)
+    }
+  ),
+  idcol = "dataset"
+)
+
+dt_seurat_org <- merge.data.table(
+  dt_genes_seurat,
+  dt_org_mm[, c("SYMBOL", "ALIAS")],
+  by.x = "gene_orig",
+  by.y = "ALIAS",
+  all.x = TRUE,
+  all.y = TRUE
+)
+dt_seurat_org <- unique(dt_seurat_org[!is.na(dataset)])
+dt_seurat_org[
+  ,
+  symbol_in_lri := SYMBOL %in% genes_lri_mouse
+]
+dt_seurat_org[
+  ,
+  n_symbol_by_orig := .N,
+  by = c("gene_orig", "dataset")
+]
+dt_seurat_org[
+  ,
+  n_orig_by_symbol := .N,
+  by = c("SYMBOL", "dataset")
+]
+
+fun_map_genes <- function(dt) {
+  n_symbol_by_orig <- n_orig_by_symbol <- gene_orig <-
+  SYMBOL <- gene_res <- NULL
+  dt_one2one <- dt[n_symbol_by_orig == 1 & n_orig_by_symbol == 1]
+  dt_one2one[, gene_res := SYMBOL]
+  dt_na <- dt[is.na(SYMBOL)]
+  dt_na[, gene_res := gene_orig]
+  dt_res <- rbindlist(
+    l = list(
+      dt_one2one[, c("gene_orig", "gene_res")],
+      dt_na[, c("gene_orig", "gene_res")]
+    )
+  )
+  dt_temp <- dt[!gene_orig %in% dt_res$gene_orig]
+  dt_sim <- dt_temp[gene_orig == SYMBOL]
+  dt_sim[, gene_res := gene_orig]
+  dt_res <- rbindlist(
+    l = list(
+      dt_res,
+      dt_sim[, c("gene_orig", "gene_res")]
+    )
+  )
+  dt_temp <- dt_temp[gene_orig != SYMBOL]
+  dt_temp <- dt_temp[!gene_orig %in% dt_res$gene_orig]
+  for (orig in unique(dt_temp$gene_orig)) {
+    if (orig %in% dt_res$gene_orig | orig %in% dt_res$gene_res) {
+        print(orig)
+        stop("internal error")
+      } else {
+        symbs <- dt_temp[gene_orig == orig]$SYMBOL
+        symbs <- symbs[!symbs %in% c(dt_res$gene_orig, dt_res$gene_res)]
+        if (length(symbs) == 0) {
+          dt_res <- rbindlist(
+            l = list(
+              dt_res,
+              data.table(gene_orig = orig, gene_res = orig)
+            )
+          )
+        } else if(length(symbs) == 1) {
+          dt_res <- rbindlist(
+            l = list(
+              dt_res,
+              data.table(gene_orig = orig, gene_res = symbs)
+            )
+          )
+        } else {
+          dt_temp_2 <- dt_temp[gene_orig == orig & SYMBOL %in% symbs][
+            symbol_in_lri == TRUE
+          ]
+          if (nrow(dt_temp_2) == 0) {
+            dt_res <- rbindlist(
+              l = list(
+                dt_res,
+                data.table(gene_orig = orig, gene_res = symbs[[1]])
+              )
+            )
+          } else {
+            dt_res <- rbindlist(
+              l = list(
+                dt_res,
+                dt_temp_2[1][, gene_res := SYMBOL][, c("gene_orig", "gene_res")]
+              )
+            )
+          }
+        }
+      }
+  }
+  return(dt_res)
+}
+
+genes_seurat_mapped <- lapply(
+  names(genes_seurat),
+  function(dat) {
+    fun_map_genes(dt_seurat_org[dataset == dat])
+  }
+)
+names(genes_seurat_mapped) <- names(genes_seurat)
+
+## Function inspired from Seurat.utils to rename genes ####
+
+rename_seurat_genes <- function(
+  obj,
+  newnames) {
+  RNA <- obj@assays$RNA
+  if (nrow(RNA) == length(newnames)) {
+    if (length(RNA@counts)) RNA@counts@Dimnames[[1]] <- newnames
+    if (length(RNA@data)) RNA@data@Dimnames[[1]] <- newnames
+  } else {
+    stop("Unequal gene sets: nrow(RNA) != nrow(newnames)")
+  }
+  obj@assays$RNA <- RNA
+  return(obj)
+}
+
+seurats_processed_renamed <- list()
+
+## Check and change gene names for TMS FACS ####
+
+table(
+  genes_seurat$tms_facs %in% genes_seurat_mapped$tms_facs$gene_orig
+)
+table(
+  genes_seurat_mapped$tms_facs$gene_orig %in% genes_seurat$tms_facs
+)
+any(duplicated(genes_seurat_mapped$tms_facs$gene_orig))
+any(duplicated(genes_seurat_mapped$tms_facs$gene_res))
+anyNA(genes_seurat_mapped$tms_facs$gene_res)
+
+table(genes_seurat$tms_facs %in% genes_lri_mouse)
+table(genes_seurat_mapped$tms_facs$gene_orig %in% genes_lri_mouse)
+table(genes_seurat_mapped$tms_facs$gene_res %in% genes_lri_mouse)
+
+genes_seurat_mapped$tms_facs <- genes_seurat_mapped$tms_facs[
+  order(match(gene_orig, rownames(seurats_processed$tms_facs)))
+]
+identical(
+  genes_seurat_mapped$tms_facs$gene_orig,
+  rownames(seurats_processed$tms_facs)
+)
+
+seurats_processed_renamed$tms_facs <- rename_seurat_genes(
+  seurats_processed$tms_facs,
+  genes_seurat_mapped$tms_facs$gene_res
+)
+
+identical(
+  rownames(seurats_processed_renamed$tms_facs),
+  genes_seurat_mapped$tms_facs$gene_res
+)
+
+## Check and change gene names for TMS Droplet ####
+
+table(
+  genes_seurat$tms_droplet %in% genes_seurat_mapped$tms_droplet$gene_orig
+)
+table(
+  genes_seurat_mapped$tms_droplet$gene_orig %in% genes_seurat$tms_droplet
+)
+any(duplicated(genes_seurat_mapped$tms_droplet$gene_orig))
+any(duplicated(genes_seurat_mapped$tms_droplet$gene_res))
+anyNA(genes_seurat_mapped$tms_droplet$gene_res)
+
+table(genes_seurat$tms_droplet %in% genes_lri_mouse)
+table(genes_seurat_mapped$tms_droplet$gene_orig %in% genes_lri_mouse)
+table(genes_seurat_mapped$tms_droplet$gene_res %in% genes_lri_mouse)
+
+genes_seurat_mapped$tms_droplet <- genes_seurat_mapped$tms_droplet[
+  order(match(gene_orig, rownames(seurats_processed$tms_droplet)))
+]
+identical(
+  genes_seurat_mapped$tms_droplet$gene_orig,
+  rownames(seurats_processed$tms_droplet)
+)
+
+seurats_processed_renamed$tms_droplet <- rename_seurat_genes(
+  seurats_processed$tms_droplet,
+  genes_seurat_mapped$tms_droplet$gene_res
+)
+
+identical(
+  rownames(seurats_processed_renamed$tms_droplet),
+  genes_seurat_mapped$tms_droplet$gene_res
+)
+
+## Check and change gene names for Calico kidney ####
+
+table(
+  genes_seurat$calico_kidney %in% genes_seurat_mapped$calico_kidney$gene_orig
+)
+table(
+  genes_seurat_mapped$calico_kidney$gene_orig %in% genes_seurat$calico_kidney
+)
+any(duplicated(genes_seurat_mapped$calico_kidney$gene_orig))
+any(duplicated(genes_seurat_mapped$calico_kidney$gene_res))
+anyNA(genes_seurat_mapped$calico_kidney$gene_res)
+
+table(genes_seurat$calico_kidney %in% genes_lri_mouse)
+table(genes_seurat_mapped$calico_kidney$gene_orig %in% genes_lri_mouse)
+table(genes_seurat_mapped$calico_kidney$gene_res %in% genes_lri_mouse)
+
+genes_seurat_mapped$calico_kidney <- genes_seurat_mapped$calico_kidney[
+  order(match(gene_orig, rownames(seurats_processed$calico_kidney)))
+]
+identical(
+  genes_seurat_mapped$calico_kidney$gene_orig,
+  rownames(seurats_processed$calico_kidney)
+)
+
+seurats_processed_renamed$calico_kidney <- rename_seurat_genes(
+  seurats_processed$calico_kidney,
+  genes_seurat_mapped$calico_kidney$gene_res
+)
+
+identical(
+  rownames(seurats_processed_renamed$calico_kidney),
+  genes_seurat_mapped$calico_kidney$gene_res
+)
+
+## Check and change gene names for Calico lung ####
+
+table(
+  genes_seurat$calico_lung %in% genes_seurat_mapped$calico_lung$gene_orig
+)
+table(
+  genes_seurat_mapped$calico_lung$gene_orig %in% genes_seurat$calico_lung
+)
+any(duplicated(genes_seurat_mapped$calico_lung$gene_orig))
+any(duplicated(genes_seurat_mapped$calico_lung$gene_res))
+anyNA(genes_seurat_mapped$calico_lung$gene_res)
+
+table(genes_seurat$calico_lung %in% genes_lri_mouse)
+table(genes_seurat_mapped$calico_lung$gene_orig %in% genes_lri_mouse)
+table(genes_seurat_mapped$calico_lung$gene_res %in% genes_lri_mouse)
+
+genes_seurat_mapped$calico_lung <- genes_seurat_mapped$calico_lung[
+  order(match(gene_orig, rownames(seurats_processed$calico_lung)))
+]
+identical(
+  genes_seurat_mapped$calico_lung$gene_orig,
+  rownames(seurats_processed$calico_lung)
+)
+
+seurats_processed_renamed$calico_lung <- rename_seurat_genes(
+  seurats_processed$calico_lung,
+  genes_seurat_mapped$calico_lung$gene_res
+)
+
+identical(
+  rownames(seurats_processed_renamed$calico_lung),
+  genes_seurat_mapped$calico_lung$gene_res
+)
+
+## Check and change gene names for Calico spleen ####
+
+table(
+  genes_seurat$calico_spleen %in% genes_seurat_mapped$calico_spleen$gene_orig
+)
+table(
+  genes_seurat_mapped$calico_spleen$gene_orig %in% genes_seurat$calico_spleen
+)
+any(duplicated(genes_seurat_mapped$calico_spleen$gene_orig))
+any(duplicated(genes_seurat_mapped$calico_spleen$gene_res))
+anyNA(genes_seurat_mapped$calico_spleen$gene_res)
+
+table(genes_seurat$calico_spleen %in% genes_lri_mouse)
+table(genes_seurat_mapped$calico_spleen$gene_orig %in% genes_lri_mouse)
+table(genes_seurat_mapped$calico_spleen$gene_res %in% genes_lri_mouse)
+
+genes_seurat_mapped$calico_spleen <- genes_seurat_mapped$calico_spleen[
+  order(match(gene_orig, rownames(seurats_processed$calico_spleen)))
+]
+identical(
+  genes_seurat_mapped$calico_spleen$gene_orig,
+  rownames(seurats_processed$calico_spleen)
+)
+
+seurats_processed_renamed$calico_spleen <- rename_seurat_genes(
+  seurats_processed$calico_spleen,
+  genes_seurat_mapped$calico_spleen$gene_res
+)
+
+identical(
+  rownames(seurats_processed_renamed$calico_spleen),
+  genes_seurat_mapped$calico_spleen$gene_res
+)
+
+## save renamed Seurat objects ####
+
+saveRDS(
+  seurats_processed_renamed$tms_facs,
+  paste0(
+    path_seurat,
+    "seurat_final_and_renamed_tms_facs.rds"
+  )
+)
+saveRDS(
+  seurats_processed_renamed$tms_droplet,
+  paste0(
+    path_seurat,
+    "seurat_final_and_renamed_tms_droplet.rds"
+  )
+)
+saveRDS(
+  seurats_processed_renamed$calico_kidney,
+  paste0(
+    path_seurat,
+    "seurat_final_and_renamed_calico_kidney_unfiltered.rds"
+  )
+)
+saveRDS(
+  seurats_processed_renamed$calico_lung,
+  paste0(
+    path_seurat,
+    "seurat_final_and_renamed_calico_lung_unfiltered.rds"
+  )
+)
+saveRDS(
+  seurats_processed_renamed$calico_spleen,
+  paste0(
+    path_seurat,
+    "seurat_final_and_renamed_calico_spleen_unfiltered.rds"
+  )
+)
+
 ## Create metadata data.table ####
 
 dt_metadata <- rbindlist(
   lapply(
-    seurats_processed,
+    seurats_processed_renamed,
     function(i) {
       as.data.table(i[[]])
     }
@@ -426,12 +784,12 @@ saveRDS(
   dt_metadata,
   paste0(
     path_scagecom_output,
-    "a1_dt_metadata.rds"
+    "a2_dt_metadata.rds"
   )
 )
 
 ## Check which cell-type annotations are all in cell-ontology ####
-
+s
 table(
   unique(dt_metadata$cell_ontology_final) %in% ontoProc::getCellOnto()$name
 )
