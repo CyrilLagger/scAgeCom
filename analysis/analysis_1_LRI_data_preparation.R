0a95b4568d7f6c491799c6801554a1442b8bdaf0
Cyril Lagger <lagger.cyril@gmail.com>
2022-12-21 03:00:30 +0000

    Read pmid file instead of connecting to NCBI

--- analysis/analysis_1_LRI_data_preparation.R
+++ analysis/analysis_1_LRI_data_preparation.R
@@ -194,12 +194,20 @@ pmid_aging <- rentrez::entrez_search(
 )
 any(duplicated(pmid_aging$ids))
 
+#if not working because of new NCBI retmax
+pmid_aging_ids <- fread(
+  paste0(
+    path_scagecom_input,
+    "aging_pmid_ncbi.txt"
+  )
+)
+
 gene2pubmed_mouse_aging <- gene2pubmed_mouse[
-  PubMed_ID %in% pmid_aging$ids &
+  PubMed_ID %in% pmid_aging_ids$V1 &
     count_PubMed_ID <= 50
 ]
 gene2pubmed_human_aging <- gene2pubmed_human[
-  PubMed_ID %in% pmid_aging$ids &
+  PubMed_ID %in% pmid_aging_ids$V1 &
     count_PubMed_ID <= 50
 ]
 
