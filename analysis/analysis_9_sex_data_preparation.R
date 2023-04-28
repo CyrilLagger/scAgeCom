ef89f3b61b7d8cc23ec3dc3e12c6b1708dc004eb
Cyril Lagger <lagger.cyril@gmail.com>
2023-04-28 14:09:26 -0700

    Add Calico cell number distribution

--- analysis/analysis_9_sex_data_preparation.R
+++ analysis/analysis_9_sex_data_preparation.R
@@ -11,10 +11,48 @@
 ####################################################
 ##
 
+## prepare Calico cell number distribution ####
+
+dt_metadata_calico <- dt_metadata[
+  grepl("calico", dataset)
+]
+dt_metadata_calico[
+  ,
+  age_group := ifelse(
+    age %in% c("young"), "YOUNG", "OLD"
+  )
+]
+dt_metadata_calico[
+  ,
+  tissue := ifelse(
+    grepl("kidney", dataset),
+    "Kidney",
+    ifelse(
+      grepl("lung", dataset),
+      "Lung",
+      "Spleen"
+    )
+  )
+]
+dt_metadata_calico[, dataset := "calico_droplet"]
+dt_metadata_calico[, sex := "male"]
+dt_md_tms_age_sex_calico <- dt_metadata_calico[
+  ,
+  .N,
+  by = c("dataset", "tissue", "cell_ontology_final", "sex", "age_group")
+][order(dataset, tissue, sex, age_group)]
+
 ## Distribution of cells across age vs sex for each tissue ####
 
-dt_md_tms_age_sex_dc <- dcast.data.table(
+dt_md_tms_age_sex_full <- rbindlist(
+  l = list(
     dt_md_tms_age_sex,
+    dt_md_tms_age_sex_calico
+  )
+)
+
+dt_md_tms_age_sex_dc <- dcast.data.table(
+  dt_md_tms_age_sex_full,
     dataset + tissue + cell_ontology_final ~ sex + age_group,
     value.var = "N",
     fill = 0
@@ -50,7 +88,11 @@ dt_md_tms_age_sex_dc[
   dataset_male := ifelse(
     dataset == "tms_facs",
     paste("TMS FACS (male)", tissue, cell_ontology_final, sep = "_"),
-    paste("TMS Droplet (male)", tissue, cell_ontology_final, sep = "_")
+    ifelse(
+      dataset == "tms_droplet",
+      paste("TMS Droplet (male)", tissue, cell_ontology_final, sep = "_"),
+      paste("Calico Droplet (male)", tissue, cell_ontology_final, sep = "_")
+    )
   )
 ]
 dt_md_tms_age_sex_dc[
@@ -58,9 +100,14 @@ dt_md_tms_age_sex_dc[
   dataset_female := ifelse(
     dataset == "tms_facs",
     paste("TMS FACS (female)", tissue, cell_ontology_final, sep = "_"),
-    paste("TMS Droplet (female)", tissue, cell_ontology_final, sep = "_")
+    ifelse(
+      dataset == "tms_droplet",
+      paste("TMS Droplet (female)", tissue, cell_ontology_final, sep = "_"),
+      paste("Calico Droplet (female)", tissue, cell_ontology_final, sep = "_")
+    )
   )
 ]
+
 dt_md_tms_age_sex_dc[
     ,
     male_scd := dataset_male %in% unique(
@@ -92,14 +139,14 @@ fwrite(
     dt_md_tms_age_sex_dc,
     paste0(
         path_scagecom_output,
-        "Supplementary_DATA_sex_distribution.csv"
+        "Supplementary_DATA_sex_distribution2.csv"
     )
 )
 
 dt_md_tms_age_sex_dc <- fread(
   paste0(
     path_scagecom_output,
-    "Supplementary_DATA_sex_distribution.csv"
+    "Supplementary_DATA_sex_distribution2.csv"
   )
 )
 
